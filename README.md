59095c9cef1e44c496e4d2a8f67354ca1d53d449
Ylva Selling <ylvse560@student.liu.se>
2021-02-22 16:13:23 +0100

    Initial commit. Iframe with WWT accepts messages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Sky Browser Integration 2021
 This is a project to integrate World Wide Telescope and OpenSpace. It is carried out at Linköping University, Sweden, as a master thesis work of a MSE with specialization in Media Technology.
 The students who work on this project are Ylva Selling and Ester Lindgren.
+
+# Set up and dependencies
+This web page is dependent on World Wide Telescope packages @wwtelescope/research-app-messages/dist/classic_pywwt and @wwtelescope/research-app. These are node_modules that need to be downloaded.
+The research app needs to be build and to be set up on a local host: http://localhost:7800 is the address right now. The files in this repo need to be on a local host: http://localhost:8000 is the address right now. The typescript file needs to be compiled into JS.
---
+++ functions.ts
@@ -0,0 +1,23 @@
+import { CenterOnCoordinatesMessage } from "WWT/node_modules/@wwtelescope/research-app-messages/dist/classic_pywwt"
+
+function setupOrigin() {
+  let frame = document.getElementsByTagName("iframe")[0].contentWindow;
+  console.log(frame);
+  //wwt.contentWindow.frames.wwt.origin = "https://web.wwtassets.org/";
+  frame.postMessage("Hello World!", "http://localhost:7800") ;
+}
+
+function changeCoords() {
+  //setupOrigin();
+  let frame = document.getElementsByTagName("iframe")[0].contentWindow;
+  const message : CenterOnCoordinatesMessage = {
+    event: "center_on_coordinates",
+    ra: Number((<HTMLInputElement>document.getElementById('ra')).value) ,
+    dec: Number((<HTMLInputElement>document.getElementById('dec')).value),
+    fov: Number((<HTMLInputElement>document.getElementById('fov')).value),
+    instant: false,
+  };
+
+  frame.postMessage(message,"http://localhost:7800");
+  console.log(message);
+}
