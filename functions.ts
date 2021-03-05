c74558a2ae2ba8c2685770f29924d9305a9cb090
Ylva Selling <ylvse560@student.liu.se>
2021-03-05 15:27:54 +0100

    Remove unneccessary test function

--- functions.ts
+++ functions.ts
@@ -1,12 +1,5 @@
 import { CenterOnCoordinatesMessage } from "./node_modules/@wwtelescope/research-app-messages/dist/classic_pywwt"
 
-function setupOrigin() {
-  let frame = document.getElementsByTagName("iframe")[0].contentWindow;
-  console.log(frame);
-  //wwt.contentWindow.frames.wwt.origin = "https://web.wwtassets.org/";
-  frame.postMessage("Hello World!", "http://localhost:8000") ;
-}
-
 function sendMessageToWWT(message) {
   //setupOrigin();
   let frame = document.getElementsByTagName("iframe")[0].contentWindow;
