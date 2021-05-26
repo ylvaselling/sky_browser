724999f3852015e56769f77a4838aa7475ad93fc
Ylva Selling <ylvse560@student.liu.se>
2021-05-26 16:30:38 +0200

    Establish connection with OpenSpace lua api and use javascript instead of typescript

---
+++ public/functions.js
@@ -0,0 +1,57 @@
+var openspace;
+const no_id = "no_id";
+var id = no_id;
+var isSynced = false;
+
+function sendMessageToWWT(message) {
+    var frame = document.getElementsByTagName("iframe")[0].contentWindow;
+    frame.postMessage(message, "http://localhost:8080");
+}
+
+// Helper function to connect to opensapce
+function connectToOpenSpace() {
+  let host = "127.0.0.1";
+  let api = window.openspaceApi(host, 4682);
+
+  api.onDisconnect(() => {
+    openspace = null;
+  });
+
+  api.onConnect(async () => {
+    try {
+      openspace = await api.library();
+      console.log('Connected to OpenSpace');
+    } catch (e) {
+      console.log('OpenSpace library could not be loaded: Error: \n', e)
+      return;
+    }
+    if(openspace) {
+      // Get ID
+      openspace.skybrowser.sendOutIdsToBrowsers();
+    }
+  })
+  // Connect
+  api.connect();
+}
+
+function setId(newId) {
+  console.log("Setting id : " + newId)
+  if(newId != id) {
+      id = newId;
+      // Ensure that a proper ID is sent to OpenSpace
+      if(id != no_id && openspace) {
+        openspace.skybrowser.initializeBrowser(id);
+      }
+  }
+}
+
+function startUp() {
+    // Listen to callback functions from WWT
+    window.addEventListener('message', function (event) {
+        if(!isSynced && id != no_id) {
+          openspace.skybrowser.loadImagesToWWT(id);
+          isSynced = true;
+        }
+    });
+
+};
--- public/functions.ts
+++
@@ -1,5 +0,0 @@
-function sendMessageToWWT(message) {
-  //setupOrigin();
-  let frame = document.getElementsByTagName("iframe")[0].contentWindow;
-  frame.postMessage(message,"http://localhost:8080");
-}
